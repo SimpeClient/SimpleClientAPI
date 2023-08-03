091f2ced1526269ac92de2b76073cffcf8524f1c
Fabi.exe <66797305+FabiPunktExe@users.noreply.github.com>
2023-08-03 17:09:16 +0200

    added documentation

--- src/main/java/simpleclient/api/SimpleClientAPI.java
+++ src/main/java/simpleclient/api/SimpleClientAPI.java
@@ -7,31 +7,60 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class SimpleClientAPI {
-    public static final String MIN_SUPPORTED_VERSION = "0.2.6";
+    /**
+     * Minimum SimpleClient version supported by the plugin
+     */
+    public static final String MIN_SUPPORTED_VERSION = "0.2.7";
     private static final Set<Player> simpleClientPlayers = new HashSet<>();
     private static final Set<Player> blockingPlayers = new HashSet<>();
     private static boolean legacyPvPEnabled = false;
 
+    /**
+     * Checks if a player is using SimpleClient
+     * @param player The player
+     * @return True if the player is using SimpleClient
+     */
     public static boolean isUsingSimpleClient(Player player) {
         return simpleClientPlayers.contains(player);
     }
 
+    /**
+     * Checks if a player is using SimpleClient and blocking with a sword
+     * @param player The player
+     * @return True if the player is using SimpleClient and blocking with a sword
+     */
     public static boolean isBlockingWithSword(Player player) {
         return isUsingSimpleClient(player) && blockingPlayers.contains(player);
     }
 
+    /**
+     * Gets the players who are using SimpleClient
+     * @return Set of players who are using SimpleClient
+     */
     public static Set<Player> getSimpleClientPlayers() {
         return simpleClientPlayers;
     }
 
+    /**
+     * Gets the players who are using SimpleClient and blocking with a sword
+     * @return Set of players who are using SimpleClient and blocking with a sword
+     */
     public static Set<Player> getBlockingPlayers() {
         return blockingPlayers;
     }
 
+    /**
+     * Weather legacy PvP is enabled or not
+     * @return True if legacy PvP is enabled
+     */
     public static boolean isLegacyPvPEnabled() {
         return legacyPvPEnabled;
     }
 
+    /**
+     * Enabled or disables legacy PvP
+     * @param enabled Weather legacy PvP should be enabled or not
+     */
     public static void setLegacyPvPEnabled(boolean enabled) {
         legacyPvPEnabled = enabled;
         if (!enabled) {
